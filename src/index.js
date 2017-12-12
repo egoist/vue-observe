557b570591884b853bd06f2fcfb410b946eff052
EGOIST <0x142857@gmail.com>
2017-12-12 23:48:43 +0800

    catch ya

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 export default function observe(obj, key, desc) {
-  if (!obj.__state) {
+  if (!obj.__$$state) {
     Vue.util.defineReactive(obj, '__$$state', {})
   }
   Vue.set(obj.__$$state, key, desc.initializer())
